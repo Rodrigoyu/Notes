12587956ec08acf024c96a623c3ca6a626e00790
Rodrigo <rodrigoj1478@gmail.com>
2024-12-19 19:01:03 -0300

    002

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,4 @@ Route::get('about', function () {
     echo"about usgit";
 });
 
+Route::get('/main', [MainController::class, "index"]);
