b3035289086e23784850355d48615b64008c1e18
Rodrigo <rodrigoj1478@gmail.com>
2025-01-12 15:41:00 -0300

    021

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\ChechIsNotLogged;
 use App\Http\Middleware\CheckIsLogged;
 use Illuminate\Support\Facades\Route;
 
 //auth Route
-
+Route::middleware([ChechIsNotLogged::class])->group(function(){
 Route::get('/login', [AuthController::class,'login']);
 Route::post('/loginSubmit', [AuthController::class,'loginSubmit']);
+});
+
 
 Route::middleware([CheckIsLogged::class])->group(function(){
     Route::get('/', [MainController::class,'index']);
