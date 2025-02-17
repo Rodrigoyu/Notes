6d8b833a2e136dec88a3b18fa4fe0ad870bcbfc9
Rodrigo <rodrigoj1478@gmail.com>
2025-02-17 20:12:10 -0300

    031

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Http\Request;
@@ -24,13 +25,43 @@ class MainController extends Controller
     }
 
     public function newNoteSubmit(Request $request){
-        echo 'hellow new Notes';
+        //validade request
+        $request->validate(
+            [
+                'text_title'=>'required|min:1|max:200', 
+                'text_note'=>'required|min:1|max:5000'
+            ],
+            [
+                'text_title.required'=>'O título é obrigadorio',
+                'text_title.min'=>'O título deve ter no minimo :min caractere',
+                'text_title.max'=>'O título deve ter no Maximo :max caractere',
+                'text_note.required'=>'A nota é obrigadorio',
+                'text_note.min'=>'Minimo de caracteres é :min',
+                'text_note.max'=>'O maximo de caracteres é :max',
+
+            ]
+        );
+        //get user id
+        $id = session('user.id');
+        //create new note
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+        //redirect to home
+        return redirect()->route('home');
+
     }
 
     public function editNote($id){
        
         $id = Operations::decryptId($id);
-       echo "I'm editing note with id = $id";
+       
+        //load note
+        $note = Note::find($id);
+        //show edit note view
+        return view('edit_note', ['note' => $note]);
     }
 
     public function deleteNote($id){
